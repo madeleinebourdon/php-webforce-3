d4a9a71408352d38ca5d8a3fd58887ed5d1d5c08
Madeleine Bourdon <madeleine.bourdon@gmail.com>
2018-10-24 15:38:19 +0200

    added customized content to vital pages

--- 13 - Pizzastore/index.php
+++ 13 - Pizzastore/index.php
@@ -2,13 +2,8 @@
 
 
 
-    <main role="main" class="container">
-
-      <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-      </div>
-
-    </main><!-- /.container -->
+    <main class="container">
+        <h1>Mon site Pizza Store</h1>
+    </main>
 
 <?php require_once(__DIR__.'/partials/footer.php'); ?>
---
+++ 13 - Pizzastore/pizza_list.php
@@ -0,0 +1,9 @@
+<?php require_once(__DIR__.'/partials/header.php'); ?>
+
+
+
+    <main class="container">
+        <h1>Liste des pizzas</h1>
+    </main>
+
+<?php require_once(__DIR__.'/partials/footer.php'); ?>
